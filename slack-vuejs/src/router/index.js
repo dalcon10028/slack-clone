afd5c431bdb8a35e57f943f188ce5141495c528d
dalcon10028 <dalcon10280@gmail.com>
2021-10-08 10:41:04 +0900

    feat: Clone slack-vue

--- slack-vuejs/src/router/index.js
+++ slack-vuejs/src/router/index.js
@@ -1,30 +1,22 @@
 import Vue from "vue";
-import VueRouter from "vue-router";
-import Home from "../views/Home.vue";
+import Router from "vue-router";
+import HelloWorld from "@/components/HelloWorld";
+import Chat from "@/components/Chat";
 
-Vue.use(VueRouter);
+Vue.use(Router);
 
-const routes = [
-  {
-    path: "/",
-    name: "Home",
-    component: Home,
-  },
-  {
-    path: "/about",
-    name: "About",
-    // route level code-splitting
-    // this generates a separate chunk (about.[hash].js) for this route
-    // which is lazy-loaded when the route is visited.
-    component: () =>
-      import(/* webpackChunkName: "about" */ "../views/About.vue"),
-  },
-];
-
-const router = new VueRouter({
+export default new Router({
   mode: "history",
-  base: process.env.BASE_URL,
-  routes,
+  routes: [
+    {
+      path: "/",
+      name: "HelloWorld",
+      component: HelloWorld,
+    },
+    {
+      path: "/channel/:cname",
+      name: "channel",
+      component: Chat,
+    },
+  ],
 });
-
-export default router;
